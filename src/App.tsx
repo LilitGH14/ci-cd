5ee80515e8e64ff6149611938f904b02bee5224a
LilitGH14 <lilitgithub@gmail.com>
2024-06-07 12:21:32 +0400

    remove some els 7

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Staging tests</p>
+        <p>Staging tests 2</p>
       </header>
     </div>
   );
